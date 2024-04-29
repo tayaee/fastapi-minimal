a51b70db655263a6c9981c0244ed9c5a4c7a0f6d
tayaee <soonki.ji@gmail.com>
2024-04-29 14:38:44 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class RequestMessage(BaseModel):
+    text: str
+
+@app.get('/')
+def index():
+  return HTMLResponse('<h1>A self-documenting web app v1</h1>')
+
+@app.post('/generate')
+def generate(request: RequesteMessage):
+  result = {
+    "errcode": 0,
+    "errmsg": "",
+    "echo": request
+  }
+  return result  
